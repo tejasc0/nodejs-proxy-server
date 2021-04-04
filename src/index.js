3e1b24914ea8bf9f667453bf837f0d2025de0cc7
tejasc0 <tejasc433@gmail.com>
2021-04-04 10:33:12 +0000

    added express.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+const express = require('express');
+const morgan = require("morgan");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 ReactDOM.render(<App/>, document.getElementById('root'));
