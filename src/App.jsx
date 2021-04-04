83e340af27ba9fe406e3b618c64c8208fc5b32b6
tejasc0 <tejasc433@gmail.com>
2021-04-04 10:28:28 +0000

    material ui added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Typography } from '@material-ui/core';
+import CSS from './App.css';
+import Box from '@material-ui/core/Box';
+
+const App = () => {
+    return (
+        <>
+              <Typography variant="h2" align="center" colorPrimary="purple" gutterBottom>
+              <Box color="primary.main">Nodejs Proxy Server</Box></Typography>
+        </>
+    )
+}
+
+export default App
